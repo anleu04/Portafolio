1c0d49b0078585cd2fa502e3bdc987dd74337d9e
anleu04 <126489889+anleu04@users.noreply.github.com>
2024-03-07 01:57:37 +0000

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,13 @@
+let menu_header = `
+    <div class="logo">RM</div>
+    <nav>
+        <div>Opción_1</div>
+        <div>Opción_2</div>
+        <div>Opción_3</div>
+        <div>Opción_4</div>
+        <div>Opción_5</div>
+    </nav>
+    <div class="btn_header">@JBANLEU</div>
+`;
+
+export { menu_header}
---
+++ js/perfil.js
@@ -0,0 +1,12 @@
+let perfil = `
+    <div class="nombre_usuario  ">
+        <div class="logo">
+            RM
+        </div>
+        <span>
+            JOSUEANLEU 
+        </span>
+    </div>
+`;
+
+export {perfil}
